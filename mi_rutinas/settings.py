629358eff308c60a38ad2e379d7916099d8a158b
Brandon-dev314 <tbdonran.23@gmail.com>
2025-07-14 00:42:03 -0600

    Update settings.py

--- mi_rutinas/settings.py
+++ mi_rutinas/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -72,6 +73,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'usuarios' / 'templates' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Para producción
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
